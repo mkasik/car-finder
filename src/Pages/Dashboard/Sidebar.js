e8699ac95ca5ec8e59e91c65704798744c038428
Mk Asik <mkasik123@gmail.com>
2022-11-26 23:48:57 +0600

    add card

--- src/Pages/Dashboard/Sidebar.js
+++ src/Pages/Dashboard/Sidebar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
 import Profile from '../Profile/Profile';
 import Seller from '../Seller/Seller';
 import './Sidebar.css'
 
 const Sidebar = () => {
+    const { user } = useContext(AuthContext);
+    // console.log('my email: ', user.email)
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/users')
+            .then(res => res.json())
+            .then(data => setUsers(data));
+    }, [])
+    console.log('all user:', users);
+    const data = users.filter(dd => dd.email === user.email);
+    console.log('this is email:', data[0].role)
+
     return (
         <div >
 
